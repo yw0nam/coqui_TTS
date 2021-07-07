e640a80fc5d515c4b910ece64c2c77f02fe3c7ba
Eren Gölge <egolge@coqui.ai>
2021-07-08 01:29:00 +0200

    Add comput_f0 field

--- TTS/tts/models/base_tts.py
+++ TTS/tts/models/base_tts.py
@@ -163,6 +163,7 @@ class BaseTTS(BaseModel):
                 outputs_per_step=config.r if "r" in config else 1,
                 text_cleaner=config.text_cleaner,
                 compute_linear_spec=config.model.lower() == "tacotron",
+                comnpute_f0=config.get("compute_f0", False),
                 meta_data=data_items,
                 ap=ap,
                 characters=config.characters,
