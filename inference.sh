4d78421547690ad5fb510eb7da90bc439775dbbc
yw0nam <spow2544@gmail.com>
2022-01-25 16:48:06 +0900

    update inferece

--- inference.sh
+++ inference.sh
@@ -1,8 +1,9 @@
-python ./TTS/bin/synthesize.py --text "行けばバイト代も出るわよ。もちろん交通費も渡す" \
-    --speakers_file_path "experiment/coqui_tts-January-21-2022_05+16AM-7f1a2378/speakers.json"\
-    --model_path "experiment/coqui_tts-January-21-2022_05+16AM-7f1a2378/best_model.pth.tar" \
-    --config_path "experiment/coqui_tts-January-21-2022_05+16AM-7f1a2378/config.json" \
+python ./TTS/bin/synthesize.py --text "a:sumimaseN.atashiwakokoiranikuwashikunaiNdesuyo" \
+    --speakers_file_path "experiment/coqui_tts-January-25-2022_02+31AM-74ae0045/speakers.json"\
+    --model_path "experiment/coqui_tts-January-25-2022_02+31AM-74ae0045/best_model.pth.tar" \
+    --config_path "experiment/coqui_tts-January-25-2022_02+31AM-74ae0045/config.json" \
     --out_path "/home/spow12/codes/TTS/coqui_TTS/output.wav"  \
-    --speaker_idx 'ムラサメ' \
-    --use_cuda "True"
-    # --list_speaker_idxs "True"
+    --speaker_idx '芳乃' \
+    # --vocoder_path "./experiment/visual_novel_melgan_256/coqui_tts-January-23-2022_10+16PM-74ae0045/best_model.pth.tar" \
+    # --vocoder_config_path "./experiment/visual_novel_melgan_256/coqui_tts-January-23-2022_10+16PM-74ae0045/config.json"
+    # --use_cuda "True" \
